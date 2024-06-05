524fba184fa09ddfc9567006a51dc9b73ab982a9
LisHack396 <lisandrav2003@gmail.com>
2024-06-05 17:36:59 -0400

    aumento de número de datos
    Se aumentó el número de datos de 100 a 500

--- scripts/graficar.py
+++ scripts/graficar.py
@@ -6,7 +6,7 @@ from scripts.limpiar import dataframe_original_limpio
 warnings.filterwarnings("ignore", "is_categorical_dtype")
 warnings.filterwarnings("ignore", "use_inf_as_na")
 
-dataset = dataframe_original_limpio().head(100)
+dataset = dataframe_original_limpio().head(500)
 
 
 def graficar():
@@ -14,18 +14,17 @@ def graficar():
     font = "Century Gothic"
     values_pie = dataset['STATUS'].value_counts()
     labels_pie = dataset['STATUS'].value_counts().index
-    fig = plt.figure('Estadísticas de productividad', figsize=(10, 6))
+    fig = plt.figure('Estadísticas de productividad', figsize=(12, 8))
     axes = fig.subplots(nrows=2, ncols=2)
     fig.suptitle("Estadísticas de productividad 1978-2022", fontsize=20, fontweight='bold', fontname=font)
     axes[0, 0].set(title="Valores de datos por periodo", xlim=(0, 1900))
-    axes[0, 1].set(title="Distribución de los valores de datos", xlim=(0, 2500))
-    axes[1, 0].set(title="Cantidad de trabajos por unidades", ylim=(0, 100))
+    axes[0, 1].set(title="Distribución de los valores de datos", xlim=(0, 2200))
+    axes[1, 0].set(title="Cantidad de trabajos por unidades", ylim=(0, 450))
     axes[1, 1].set(title="Porcentaje de status")
-    sns.lineplot(data=dataset, x='Data_value', y='Period', ax=axes[0, 0])
     
+    sns.lineplot(data=dataset, x='Data_value', y='Period', ax=axes[0, 0])
     sns.kdeplot(dataset['Data_value'], fill=True, color='blue', ax=axes[0, 1])
     sns.rugplot(dataset['Data_value'], color='blue', ax=axes[0, 1])
-    
     sns.countplot(data=dataset, x='UNITS', ax=axes[1, 0])
     
     axes[1, 1].pie(x=values_pie, labels=labels_pie, autopct='%.2f', shadow=True)
