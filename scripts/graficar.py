22dea953903d9020e79d0ae1bd7f6cab7d602360
LisHack396 <lisandrav2003@gmail.com>
2024-06-05 16:54:51 -0400

    Update graficar.py

--- scripts/graficar.py
+++ scripts/graficar.py
@@ -6,19 +6,20 @@ from scripts.limpiar import dataframe_original_limpio
 warnings.filterwarnings("ignore", "is_categorical_dtype")
 warnings.filterwarnings("ignore", "use_inf_as_na")
 
-dataset = dataframe_original_limpio().head(50)
-values_pie = dataset['STATUS'].value_counts()
-labels_pie = dataset['STATUS'].value_counts().index
+dataset = dataframe_original_limpio().head(100)
+
 
 def graficar():
     """Graficar las estadisticas de productividad"""
-    plt.style.use('ggplot')
-    fig = plt.figure('Estadisticas de productividad', figsize=(10, 6))
+    font = "Century Gothic"
+    values_pie = dataset['STATUS'].value_counts()
+    labels_pie = dataset['STATUS'].value_counts().index
+    fig = plt.figure('Estadísticas de productividad', figsize=(10, 6))
     axes = fig.subplots(nrows=2, ncols=2)
-    fig.suptitle("Estadisticas de productividad 1978-2022", fontsize=15, fontweight='bold')
-    axes[0, 0].set(title="Valores de datos por periodo", xlim=(0, 1500))
-    axes[0, 1].set(title="Distribucion de los valores de datos", xlim=(0, 2500))
-    axes[1, 0].set(title="Cantidad de trabajos por unidades", ylim=(0, 50))
+    fig.suptitle("Estadísticas de productividad 1978-2022", fontsize=20, fontweight='bold', fontname=font)
+    axes[0, 0].set(title="Valores de datos por periodo", xlim=(0, 1900))
+    axes[0, 1].set(title="Distribución de los valores de datos", xlim=(0, 2500))
+    axes[1, 0].set(title="Cantidad de trabajos por unidades", ylim=(0, 100))
     axes[1, 1].set(title="Porcentaje de status")
     sns.lineplot(data=dataset, x='Data_value', y='Period', ax=axes[0, 0])
     
